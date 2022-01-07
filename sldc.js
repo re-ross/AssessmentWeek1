4185ff9a00a1ecbb892bb3553e2427e0d7ce5ff9
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-07 09:53:41 -0600

    defined stages of SDLC

--- sldc.js
+++ sldc.js
@@ -27,3 +27,23 @@ Maintain
 const planning =
 
   "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+  
+  const analysisOfRequirements =
+    
+    "This is the second step, during which relevant information is collected from customer for requirements by analysts. Analysis is done to check feasibility and clear ambiguity. Software Requirement Specification is then created.";
+
+const design =
+
+    "This is the third step, during which SRS is used as input and the software architecture that is used for implementing system development is derived.";
+
+const implementation =
+
+    "This is the fourth step, during which the developer uses the software architecture to build the project. All of the components of the software should be implemented at this stage."
+
+const testingAndIntegration =
+
+    "This is the fifth step, during which the developed software is tested thoroughly. Any defects that are found are fixed and the software is retested. After testing, the product is then deployed to go live."
+
+const Maintenance =
+
+    "This is the sixth step, during which the maintenance of the product is taken care of by developers. The developers check for any issues or bugs that may arise or implement any enhancements."
