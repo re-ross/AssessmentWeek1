8d20f5d383db0e2674d06d5705b1b18545e85756
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-07 11:06:33 -0600

    succesful in checking password length

--- password.js
+++ password.js
@@ -6,16 +6,13 @@ const reader = readline.createInterface({
 });
 
 reader.question("Please enter password", function (input) {
-  tokens = input.split();
+  tokens = String(input);
 
   userPass = tokens;
-  console.log(userPass);
-  console.log("--------");
+
   if (userPass.length < 10) {
-    console.log(userPass.length);
-    console.log("--------");
     console.log("Not enough characters");
-  } else if (userPass.length >= 10) {
+  } else if (userPass.length > 9) {
     console.log("Success!");
   } else console.log("Failure!");
 });
