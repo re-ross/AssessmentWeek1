30565cc848951fbf65d80834c1fd9dfe4b074b56
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-07 09:57:45 -0600

    finished variables assessment

--- variables.js
+++ variables.js
@@ -1,18 +1,21 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE 
+//CODE HERE
+
+let myName = "Ryan";
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+let faveNum = 23;
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
-
+let lovesCode = true;
 //////////////////PROBLEM 4////////////////////
 /*
   Write an if-statement that checks if the faveNum variable is 
@@ -21,6 +24,9 @@
 */
 
 //CODE HERE
+if (faveNum === 13) {
+  console.log("Lucky 13!");
+} else console.log("Not lucky 13.");
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -28,3 +34,6 @@
 */
 
 //CODE HERE
+for (i = 0; i < 5; i++) {
+  console.log(faveNum);
+}
