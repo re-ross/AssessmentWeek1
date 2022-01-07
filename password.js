8498cd6661a7c8cc28ed3a4d71566fc6ada1dfae
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-07 12:09:14 -0600

    added additional check

--- password.js
+++ password.js
@@ -7,13 +7,41 @@ const reader = readline.createInterface({
 
 reader.question("Hello, please enter a password", function (input) {
   password = String(input);
-  if ((password = "password")) {
+  let symbolCheck = password.includes(
+    "~",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "+",
+    "?"
+  );
+  console.log(symbolCheck);
+  if (password === "password") {
     console.log("Be a little more creative, please.");
   } else if (password.length < 10) {
     console.log(`Failure:
     
  ________________________________________
 < Not enough characters >
+ ----------------------------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||`);
+  } else if (password.length > 9 && symbolCheck === false) {
+    console.log(`Error:
+    
+________________________________________
+< Only letters and numbers please. >
  ----------------------------------------
         \\   ^__^
          \\  (oo)\\_______
