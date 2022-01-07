de41c01c777c25ea161bf34e8ed159022c8a1267
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-07 12:15:16 -0600

    added additional check

--- password.js
+++ password.js
@@ -23,7 +23,8 @@ reader.question("Hello, please enter a password", function (input) {
     "+",
     "?"
   );
-  console.log(symbolCheck);
+  let numberCheck = password.includes(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+  console.log(numberCheck);
   if (password === "password") {
     console.log("Be a little more creative, please.");
   } else if (password.length < 10) {
@@ -37,11 +38,22 @@ reader.question("Hello, please enter a password", function (input) {
             (__)\\       )\\/\\
                 ||----w |
                 ||     ||`);
+  } else if (password.length > 9 && numberCheck === true) {
+    console.log(`Error:
+    
+________________________________________
+< No numbers please. >
+ ----------------------------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||`);
   } else if (password.length > 9 && symbolCheck === false) {
     console.log(`Error:
     
 ________________________________________
-< Only letters and numbers please. >
+< No symbols please. >
  ----------------------------------------
         \\   ^__^
          \\  (oo)\\_______
